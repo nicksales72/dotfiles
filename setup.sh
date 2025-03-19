607dddb1836cb41106524eeb5ba7ee841d96d0ff
nick <nicksales72@gmail.com>
2025-03-18 22:32:56 -0400

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+git config --global user.name "nick"
+git config --global user.email "nicksales72@gmail.com"
+git config --global init.defaultBranch main
+git config --global pull.rebase false
+ssh-keygen -t ed25519
+cat ~/.ssh/id_ed25519.pub
+
+sudo dnf copr enable pgdev/ghostty
+sudo dnf install fastfetch ghostty
+sudo dnf update && sudo dnf upgrade
