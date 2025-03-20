c16b5daf9412ba4f9bb8636d8650f31e96a62ab4
nick <nicksales72@gmail.com>
2025-03-20 14:17:29 -0400

    Updated 2025-03-20

--- setup.sh
+++ setup.sh
@@ -7,12 +7,6 @@ git config --global pull.rebase false
 ssh-keygen -t ed25519
 cat ~/.ssh/id_ed25519.pub
 
-echo "[gemfury-nushell]
-name=Gemfury Nushell Repo
-baseurl=https://yum.fury.io/nushell/
-enabled=1
-gpgcheck=0
-gpgkey=https://yum.fury.io/nushell/gpg.key" | sudo tee /etc/yum.repos.d/fury-nushell.repo
 sudo dnf copr enable pgdev/ghostty
-sudo dnf install -y fastfetch ghostty nushell
+sudo dnf install -y fastfetch ghostty
 sudo dnf update && sudo dnf upgrade
