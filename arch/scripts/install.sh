e4172d1815d603a18fc1d80a1b863d2888030829
nicksales72 <nicksales72@gmail.com>
2025-08-08 20:00:07 -0400

    finally hyprland

--- arch/scripts/install.sh
+++ arch/scripts/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo pacman -S tlp zathura zathura-pdf-mupdf discord rofi ghostty neovim mpv fastfetch sway swaylock swayidle swaybg feh iwd pavucontrol pipewire wireplumber bluez bluez-utils texlive-basic r python-pip xournalpp dosfstools
+sudo pacman -S tlp zathura zathura-pdf-mupdf discord rofi ghostty neovim mpv fastfetch feh pavucontrol texlive-basic r python-pip xournalpp dosfstools hyprland hypridle waybar nodejs wl-clipboard fish openssh
 
 sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
 yay -S vial-appimage brave-bin 
