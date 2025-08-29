200a33911f83ed789b0a9b0794e218a50f5589e7
nicksales72 <nicksales72@gmail.com>
2025-08-28 23:33:28 -0400

    Updated 2025-08-28

--- arch/scripts/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo pacman -S tlp zathura zathura-pdf-mupdf discord rofi ghostty mpv fastfetch feh pavucontrol texlive-basic r python-pip dosfstools hyprland hyprlock hypridle hyprpaper waybar nodejs wl-clipboard fish openssh wireless_tools 
-
-sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
-yay -S vial-appimage brave-bin visual-studio-code-bin
