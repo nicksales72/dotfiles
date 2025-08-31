99022902237941990c2fb53365726b29df85bcb4
nicksales72 <nicksales72@gmail.com>
2025-08-30 21:53:10 -0400

    Updated 2025-08-30

---
+++ fedora/scripts/removebloat.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo dnf remove kmahjongg kmines kpat kolourpaint skanpage akregator kmail krdc krfb ktnef neochat pim-data-exporter pim-sieve-editor dragon elisa-player kamoso qrca ark filelight dolphin kwalletmanager5 kcalc kcharselect kfind kgpg kmouth korganizer kaddressbook kwrite libreoffice\*
