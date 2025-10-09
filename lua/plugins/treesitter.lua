e5f741affc5e6d12d3d23a703fb56928fbc58b54
nicksales72 <nicksales72@gmail.com>
2025-10-09 17:52:58 -0400

    Updated 2025-10-09

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,35 @@
+return {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    config = function()
+        require('nvim-treesitter.configs').setup({
+            ensure_installed = {
+                "lua",
+                "python",
+                "c",
+                "cpp",
+                "rust",
+                "haskell",
+                "markdown",
+                "vim",
+                "vimdoc",
+            },
+            auto_install = true,
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = false,
+            },
+            indent = { enable = true },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "<c-space>",
+                    node_incremental = "<c-space>",
+                    scope_incremental = "<c-s>",
+                    node_decremental = "<M-space>",
+                },
+            },
+        })
+    end
+}
+
