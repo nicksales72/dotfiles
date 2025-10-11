e2ffa2df5a62abd709f23f86eefc3516e9f1868d
nicksales72 <nicksales72@gmail.com>
2025-10-11 15:14:24 -0400

    Updated 2025-10-11

--- init.lua
+++ init.lua
@@ -1,8 +1,9 @@
 require("config.lazy")
 vim.lsp.enable('pyright')
 
-vim.opt.clipboard = "unnamedplus"   
+vim.cmd.colorscheme("habamax")
 vim.wo.relativenumber = true
+vim.opt.clipboard = "unnamedplus"   
 vim.opt.spell = true
 vim.opt.spelllang = "en_ca"
 vim.opt.tabstop = 4
