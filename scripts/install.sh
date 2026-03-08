be113087af58d6905ea3d5fde446f85a287308c1
nicksales72 <nicksales72@gmail.com>
2026-03-08 10:36:29 -0400

    hello

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,3 @@
  #!/bin/bash
 
- sudo pacman -S fastfetch tlp htop neovim gnome-keyring ghostty zsh brightnessctl
+ sudo pacman -S fastfetch tlp htop neovim gnome-keyring ghostty zsh brightnessctl discord calibre
