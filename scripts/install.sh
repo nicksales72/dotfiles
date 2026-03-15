15dcf408cdf3514642406be3de8ce32c58905be5
nicksales72 <nicksales72@gmail.com>
2026-03-15 17:18:06 -0400

    lsdfkjfdslofiosdjf

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,4 @@
  #!/bin/bash
 
- sudo dnf install fastfetch tlp htop neovim zsh calibre bat
+sudo dnf install fastfetch tlp htop neovim zsh calibre bat texlive-scheme-full zathura zathura-pdf-mupdf
+
