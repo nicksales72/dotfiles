cfecce1c684ee0bac37b46650d540389f52c7431
nicksales72 <nicksales72@gmail.com>
2026-06-24 15:20:05 -0400

    nvim

---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,34 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    opts = {
+        ensure_installed = {
+            "lua",
+            "python",
+            "c",
+            "cpp",
+            "rust",
+            "haskell",
+            "markdown",
+            "vim",
+            "vimdoc",
+            "lean",
+        },
+        auto_install = true,
+        highlight = {
+            enable = true,
+            additional_vim_regex_highlighting = false,
+        },
+        indent = { enable = true },
+        incremental_selection = {
+            enable = true,
+            keymaps = {
+                init_selection = "<c-space>",
+                node_incremental = "<c-space>",
+                scope_incremental = "<c-s>",
+                node_decremental = "<M-space>",
+            },
+        },
+    },
+}
+
